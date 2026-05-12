dc44a01287e8e2fbae0a1e55f0e97a27b8134117
mylinh123-tech <mylinhtayvinh@gmail.com>
2026-05-12 17:12:30 +0700

    modify code

--- src/test/java/com/linhtest/firstpackage/SecondClass.java
+++ src/test/java/com/linhtest/firstpackage/SecondClass.java
@@ -4,12 +4,11 @@ import org.testng.annotations.Test;
 
 public class SecondClass {
     @Test(groups = "smoke")
-    public void firstTest() {
-        System.out.println("2. First test method");
+    public void firstTest(){
+        System.out.println("2.First test method ");
     }
-
     @Test
-    public void secondTest() {
-        System.out.println("2. Second test method");
+    public void secondTest(){
+        System.out.println("2. Seconde test method");
     }
 }
